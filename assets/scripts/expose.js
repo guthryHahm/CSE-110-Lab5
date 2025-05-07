6efebec0a2f3efd603196c4c7b5c33594080b52b
Guthry <ghahm@ucsd.edu>
2025-05-07 02:55:30 -0700

    setup and media selection

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -3,5 +3,14 @@
 window.addEventListener('DOMContentLoaded', init);
 
 function init() {
+  const expose = document.getElementById("expose");
+  const horn_selection = expose.querySelector("#horn-select");
+  const horn_image = expose.querySelector("img");
+  const horn_audio = expose.querySelector("audio");
+
+  horn_selection.addEventListener("change",(event) => {
+    horn_image.src = "assets/images/" + event.target.value + ".svg";
+    horn_audio.src = "assets/audio/" + event.target.value + ".mp3";
+  });
   // TODO
 }
