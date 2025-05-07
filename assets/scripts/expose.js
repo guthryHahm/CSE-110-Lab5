4f1bf2ebf8f43a8e28d020d851e79440e7a94af5
Guthry <ghahm@ucsd.edu>
2025-05-07 03:27:00 -0700

    volume graphic changes

--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', init);
 
 function init() {
   const expose = document.getElementById("expose");
+  ////////////////////////// Horn ///////////////////////////
   const horn_selection = expose.querySelector("#horn-select");
   const horn_image = expose.querySelector("img");
   const horn_audio = expose.querySelector("audio");
@@ -12,5 +13,23 @@ function init() {
     horn_image.src = "assets/images/" + event.target.value + ".svg";
     horn_audio.src = "assets/audio/" + event.target.value + ".mp3";
   });
-  // TODO
+  ////////////////////////// Audio ///////////////////////////
+
+  const volume_controls = expose.querySelector("#volume-controls");
+  const volume = volume_controls.querySelector("#volume");
+  const volume_img = volume_controls.querySelector("img");
+  volume.addEventListener("input",(event) => {
+    const volume_value = event.target.value;
+    let level;
+    if (volume_value == 0){
+      level = 0;
+    } else if (volume_value < 33) {
+      level = 1;
+    } else if (volume_value < 67) {
+      level = 2;
+    } else {
+      level = 3;
+    }
+    volume_img.src = "assets/icons/volume-level-" + level + ".svg";
+  })
 }
